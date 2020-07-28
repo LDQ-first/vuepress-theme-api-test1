46f8678558b422d487f500408c5b780f32d3dda7
LDQ-first <178848890@qq.com>
2020-07-29 00:42:11 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,190 @@
 
 
+const getHomeSidebar = (title) => ({
+    children: [
+        {
+            title,
+            to: '/'
+        }
+    ]
+})
+
+const getStartSidebar = (title) => ({
+    children: [{
+        title,
+        to: '/getting-started/',
+        collapsable: false,
+        headers: [
+            {
+                title: '构建于',
+                slug: '构建于', // 当前页面的 id 锚点。
+            },
+            {
+                title: '环境依赖',
+                slug: '环境依赖', // 当前页面的 id 锚点。
+            },
+            {
+                title: '应用主题',
+                slug: '应用主题', // 当前页面的 id 锚点。
+            },
+        ],
+    }]
+})
+
+
+/* const getConfigurationsSidebar = (title) => ({
+    children: [{
+        title,
+        to: '/configurations/',
+        collapsable: false,
+        headers: [
+            {
+                title: '配置文件',
+                slug: '配置文件', // 当前页面的 id 锚点。
+            },
+            {
+                title: 'VuePress 配置',
+                slug: 'VuePress 配置', // 当前页面的 id 锚点。
+            },
+            {
+                title: '首页配置',
+                slug: '首页配置', // 当前页面的 id 锚点。
+            },
+            {
+                title: '侧边导航配置',
+                slug: '侧边导航配置', // 当前页面的 id 锚点。
+            },
+            {
+                title: '页面布局',
+                slug: '页面布局', // 当前页面的 id 锚点。
+            },
+            {
+                title: '多语言配置',
+                slug: '多语言配置', // 当前页面的 id 锚点。
+            },
+        ],
+    }]
+}) */
+
+const getConfigurationsSidebar = (titleA, titleB, titleC) => ({
+    children: [
+        {
+            title: titleA,
+            to: '/configurations/',
+            headers: [
+                {
+                    title: '配置文件',
+                    slug: '配置文件', // 当前页面的 id 锚点。
+                },
+                {
+                    title: 'VuePress 配置',
+                    slug: 'vuepress-配置', // 当前页面的 id 锚点。
+                },
+                {
+                    title: '首页配置',
+                    slug: '首页配置', // 当前页面的 id 锚点。
+                },
+                {
+                    title: '侧边导航配置',
+                    slug: '侧边导航配置', // 当前页面的 id 锚点。
+                },
+                {
+                    title: '页面布局',
+                    slug: '页面布局', // 当前页面的 id 锚点。
+                },
+                {
+                    title: '多语言配置',
+                    slug: '多语言配置', // 当前页面的 id 锚点。
+                },
+            ],
+        },
+        getComponentsSidebar(titleB),
+        getTemplateSidebar(titleC)
+    ]
+})
+
+
+/* const getComponentsSidebar = (title) => ({
+    children: [{
+        title,
+        to: '/configurations/components',
+        collapsable: false,
+        headers: [
+            {
+                title: 'Block 组件',
+                slug: 'Block 组件', // 当前页面的 id 锚点。
+            },
+            {
+                title: 'Example 组件',
+                slug: 'Example 组件', // 当前页面的 id 锚点。
+            },
+            {
+                title: 'CURL 组件',
+                slug: 'CURL 组件', // 当前页面的 id 锚点。
+            },
+            {
+                title: 'Button 组件',
+                slug: 'Button 组件', // 当前页面的 id 锚点。
+            },
+            {
+                title: 'Section 组件',
+                slug: 'Section 组件', // 当前页面的 id 锚点。
+            },
+        ],
+    }]
+}) */
+
+const getComponentsSidebar = (title) => ({
+    title,
+    to: '/configurations/components.html',
+    headers: [
+        {
+            title: 'Block 组件',
+            slug: 'block-组件', // 当前页面的 id 锚点。
+        },
+        {
+            title: 'Example 组件',
+            slug: 'example-组件', // 当前页面的 id 锚点。
+        },
+        {
+            title: 'CURL 组件',
+            slug: 'curl-组件', // 当前页面的 id 锚点。
+        },
+        {
+            title: 'Button 组件',
+            slug: 'button-组件', // 当前页面的 id 锚点。
+        },
+        {
+            title: 'Section 组件',
+            slug: 'section-组件', // 当前页面的 id 锚点。
+        },
+    ],
+})
+
+
+
+/* const getTemplateSidebar = (title) => ({
+    children: [
+        {
+            title,
+            to: '/configurations/template'
+        }
+    ]
+}) */
+
+const getTemplateSidebar = (title) => ({
+    title,
+    to: '/configurations/template/'
+})
+
+
+
+
+
+
+
+
+
 module.exports = {
     title: 'Hello VuePress',
     description: '📦 🎨 A api-friendly theme for VuePress.',
@@ -8,5 +193,17 @@ module.exports = {
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1' }],
         ['meta', { charset: 'utf-8' }],
     ],
+    themeConfig: {
+        /* sidebarGroupOrder: [
+            'getting-started', // 分组名称，即文件夹名称
+            'configurations'
+        ], */
+        sidebar: {
+            'home': getHomeSidebar('主页'),
+            'getting-started': getStartSidebar('主题起步'),
+            'configurations': getConfigurationsSidebar('基本配置', '内置组件', '文件模板'),
+        },
+        
+    }
 }
 
--- docs/README.md
+++ docs/README.md
@@ -7,3 +7,39 @@ actionLink: /getting-started
 footer: Open Source on [GitHub](https://github.com/sqrthree/vuepress-theme-api), Made by [@sqrthree](https://github.com/sqrthree), Power by [vuepress](https://github.com/vuejs/vuepress).
 ---
 
+## 1、2、3 很简单
+
+```bash
+# 安装 vuepress
+yarn global add vuepress # OR npm install -g vuepress
+
+# 安装主题
+yarn global add vuepress-theme-api # OR npm install -g vuepress-theme-api
+
+# 创建一个 md 文件，试着写一些东西吧。
+echo '# Hello, World.' > Hello.md
+
+# 启动本地开发环境服务器
+vuepress dev
+
+# 构建静态文件
+vuepress build
+```
+
+::: tip
+VuePress 要求 [Node.js](http://nodejs.org/) 版本必须 >= 8。
+:::
+
+<Section>
+
+## 为 RESTful API 而生
+
+一个简洁易用的 VuePress 主题。
+
+开箱即用，你需要做的只是安装它，然后写吧。
+
+<br>
+
+<Button type="light" to="/getting-started/">开始出发</Button>
+
+</Section>
